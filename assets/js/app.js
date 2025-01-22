2def905a8e0f5c512ebcba2638a30c5aa68e0ec3
Risa Goto <omusubi5656dup@gmail.com>
2025-01-22 12:11:34 +0900

    add:デプロイ設定

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,4 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {//particles-jsがparticles.jsonを読み込む関数
+particlesJS.load('particles-js', 'assets/js/particles.json', function() {//particles-jsがparticles.jsonを読み込む関数
     console.log('callback - particles-js config loaded');
   });
 
